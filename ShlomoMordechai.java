c893e2cd9109e83fe571f961b6dad2464d783024
shlomomor <shlomomor13@gmail.com>
2026-04-18 14:17:35 +0300

    Updated Hello.java print for Jenkins trigger

--- ShlomoMordechai.java
+++ ShlomoMordechai.java
@@ -1,7 +1,7 @@
 public class ShlomoMordechai {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            System.out.println("This is Shlomo Mordechai!");
+            System.out.println("This is Shlomo Mordechai! changed output");
         }
     }
 }
